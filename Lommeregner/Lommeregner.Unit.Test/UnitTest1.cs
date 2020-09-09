173ebb7089cf10c21ed286b4cd9dced2c1edb332
thomasgl710 <thomasgl10@gmail.com>
2020-09-09 09:59:53 +0200

    Tilfojet metoderne

--- Lommeregner/Lommeregner.Unit.Test/UnitTest1.cs
+++ Lommeregner/Lommeregner.Unit.Test/UnitTest1.cs
@@ -14,5 +14,22 @@ namespace Lommeregner.Unit.Test
         {
             Assert.Pass();
         }
+
+
+        [Test]
+        public void Add_AddingTwoPositiveIntegers_NItemIsCorrect()
+        {
+          /*  //Arrange
+            var uut = new Program();
+
+            //Act
+            var testResult = uut.Add(2, 4);
+            var expectedResult = 2 + 4;
+
+            // Assert
+            Assert.That(testResult, Is.EqualTo(expectedResult));
+            */
+
+        }
     }
 }
--- Lommeregner/Lommeregner/Program.cs
+++ Lommeregner/Lommeregner/Program.cs
@@ -8,6 +8,32 @@ namespace Lommeregner
         {
             Console.WriteLine("Hello Thomas!");
         }
+        public double Add(double a, double b)
+        {
+            double c;
+            c = a + b;
+            return c;
+        }
+        public double Subtract(double a, double b)
+        {
+            double c;
+            c = a - b;
+            return c;
+        }
+        public double Multiply(double a, double b)
+        {
+            double c;
+            c = a * b;
+            return c;
+        }
+        public double Power(double x, double exp)
+        {
+            double c;
+            c = Math.Pow(x, exp);
+            return c;
+        }
 
     }
+
 }
+
