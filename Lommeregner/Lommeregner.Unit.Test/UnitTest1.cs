7d4d376cbc203bfc159ed0c1c0b9d2a1a2a10c86
thomasgl710 <thomasgl10@gmail.com>
2020-09-10 13:02:29 +0200

    Aendringer i reference

--- Lommeregner/Lommeregner.Unit.Test/UnitTest1.cs
+++ Lommeregner/Lommeregner.Unit.Test/UnitTest1.cs
@@ -1,40 +1,47 @@
 using NUnit.Framework;
+
 using System;
 using Lommeregner;
-
 namespace Lommeregner.Unit.Test
 {
+    [TestFixture]
     public class Tests
     {
        
         [SetUp]
         public void Setup()
         {
+            var uut = new Calculator();
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(3,2,5)]
+        public void Add_AddingTwoPositiveIntegers_NItemIsCorrect()
         {
+            //Arrange
+            var uut = new Calculator();
+            
+
+            //Act
+            double numberA = 3;
+           
+            var testResult = uut.Add(numberA);
+            
+            var expectedResult = numberA;
+
+            //Assert
+            Assert.That(testResult, Is.EqualTo(expectedResult));
             Assert.Pass();
         }
 
 
         [Test]
-        public void Add_AddingTwoPositiveIntegers_NItemIsCorrect()
+        public void Add_AddingOneNegativeIntegerAndOnePositiveInteger_NItemIsCorrect()
         {
             //Arrange
-            
-            /*var uut = new Calculator();
 
             //Act
-            var testResult = uut.Add(2, 4);
-            var expectedResult = 2 + 4;
 
-            // Assert
-            Assert.That(testResult, Is.EqualTo(expectedResult));
-            */
-            
-
+            //Assert
         }
     }
 }
